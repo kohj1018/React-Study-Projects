0c065d5e56fea66d0001e74a946942dd3c197684
kohj1018 <kohj1018@gmail.com>
2021-10-24 16:01:15 +0900

    조건문

--- 04_react-basic/src/App.js
+++ 04_react-basic/src/App.js
@@ -1,23 +1,21 @@
-import React, { useState } from 'react';
-import Counter from './components/Counter';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [buttonName, setButtonName] = useState('클릭');
-  const clickButton =  () => {
-    if (buttonName === '클릭') {
-      setButtonName('click2')
-    } else {
-      setButtonName('클릭')
-    }
-  };
+  const [condition, setCondition] = useState(false);
+  const toggle = () => setCondition(!condition);
+  useEffect(() => {
+    console.log(condition);
+  }, [condition])
+  const renderCondition = condition
+    ? 'True'
+    : 'False'
   return (
     <div className="App">
       <h1>Byoungwook Koh</h1>
-      <Counter click='click1'/>
-      <Counter click={buttonName}/>
-      <Counter/>
-      <br />
-      <button onClick={clickButton}>버튼 이름 변경</button>
+      <div>
+        {renderCondition}
+      </div>
+      <button onClick={toggle}>Toggle</button>
     </div>
   );
 }
